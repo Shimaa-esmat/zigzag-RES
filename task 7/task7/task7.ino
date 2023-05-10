45e052162af69a34c7fbdb91cb4fd1e5ff136ce5
Shimaa-esmat~ <shymaaesam01@gmail.com>
2023-05-10 21:01:02 +0200

    lcd

---
+++ task 7/task7/task7.ino
@@ -0,0 +1,35 @@
+#include <LiquidCrystal.h>
+#include <Keypad.h>
+
+const byte rows = 4;
+const byte cols = 4;
+char keys[rows][cols] = {
+  {'1','2','3','A'},
+  {'4','5','6','B'},
+  {'7','8','9','C'},
+  {'*','0','#','D'}
+};
+
+
+byte rowpins[rows] = {13,12,11,10};
+byte colpins[cols] = {9,8,7,6};
+LiquidCrystal lcd(A0,A1,A2,A3,A4,A5);
+Keypad keypad = Keypad (makeKeymap (keys),rowpins ,colpins, rows, cols);
+
+void setup() {
+ lcd.begin(16,2);
+ lcd.setCursor(0,0);
+ 
+}
+
+void loop() {
+ char key = keypad.getKey();
+
+ lcd.setCursor(0,0);
+ lcd.print("press char    ");
+if (key) {
+  lcd.setCursor(0,1);
+  lcd.print(key);
+}
+
+}
